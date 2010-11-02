f7caf4c3093641b3f08fb407f445cdb0731b2a53
knevcher <knevcher@gmail.com>
2010-11-02 17:07:31 +0300

    -- parser now use regexps

--- src/parser.sh
+++ src/parser.sh
@@ -1,50 +1,48 @@
-gawk -v delta_time=$1 -v request_mask=$2 '
+gawk -v date_format=$1 -v delta_time=$2 -v app=$3 -v action="$4" -v request_status=$5 '
 BEGIN{
+  action_regexp="^"action"$"
   now_time = systime()
-  now_date = strftime("%Y%m%d%H%M", now_time)
+  now_date = strftime(date_format, now_time)
   begin_time = now_time - delta_time
   count = 0
-
-  less_500ms = 0
-  between_500ms_1000ms = 0
-  greater_1000ms = 0
+  count_fine = 0
+  count_normal = 0
+  count_bad = 0
 }
 {
-  log_time = $5
+  # example log entry
+  # hh-favresumes PUT /resume/open/1282694867?empinfo=emp(1282694862),mng(1282694865) HTTP/1.1 0.106 1288686990.805 200
+  match($0, /([^ ]+) (GET|POST|PUT|DELETE) ([^?]+)[^ ]+ HTTP\/[^ ]+ ([^ ]+) ([^ ]+) ([^ ]+)/, matches)
 
-  if(log_time >= begin_time)
-  {
-    split($12, url, "?");
+  log_entry_app = matches[1]
+  if(log_entry_app != app)
+    next
 
-    is_ids = match(url[1], /\/(([0-9]+)(,|))+/)
-    if(is_ids)
-      gsub(/\/(([0-9]+)(,|))+/, "/IDS", url[1]);
+  log_time = matches[5]
+  if(log_time <=begin_time)
+    next
 
-    is_params = match(url[1], /\/([0-9a-zA-Z]+),(([0-9a-zA-Z]+)(,|))+/)
-    if(is_params && !is_ids)
-      gsub(/\/([0-9a-zA-Z]+),(([0-9a-zA-Z]+)(,|))+/, "/PARAMS", url[1]);
+  url = matches[3]
+  if(match(url, action_regexp) == 0)
+    next
 
-    current_url = url[1]
-    status = $11
+  status = matches[2]
+  if(request_status && request_status != status)
+    next
 
-    current_request = substr(status""current_url, 2)
-    if(current_request == request_mask)
-    {
-      count += 1;
+  count += 1;
 
-      if($4 > 1)
-        greater_1000ms += 1
-      else if($4 > 0.5)
-        between_500ms_1000ms += 1
-      else
-        less_500ms += 1
-    }
-  }
+  request_time = matches[4]
+  if(request_time > 0.1)
+    count_bad += 1
+  else if(request_time > 0.05)
+    count_normal += 1
+  else
+    count_fine += 1
 }
 END{
   if(count > 0)
   {
-    # print "count:"count" less_500ms:"less_500ms" between_500ms_1000ms:"between_500ms_1000ms" greater_1000ms:"greater_1000ms;
-    print now_date","count","less_500ms","between_500ms_1000ms","greater_1000ms
+    print now_date","count","count_fine","count_normal","count_bad
   }
 }'
